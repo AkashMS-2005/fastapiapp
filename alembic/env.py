14125a816705a7399d70da0c01fb031c5c166c81
Akash M S <akashmsaiml@gmail.com>
2026-06-29 12:08:55 +0530

    company-job

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +19,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from database import Base
+from models.company import Company
+from models.job import Job
+target_metadata = Base.metadata
+
+
+
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
