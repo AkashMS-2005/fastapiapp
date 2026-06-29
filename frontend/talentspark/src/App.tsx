68675d6970fe16483890132446280335b6e3a428
Akash M S <akashmsaiml@gmail.com>
2026-06-29 16:43:24 +0530

    frontend

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,6 +1,9 @@
 import NavBar from "./components/NavBar";
 import Welcome from "./components/Welcome";
 import Footer from "./components/Footer";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+
 
 function App()
 {
@@ -8,6 +11,8 @@ function App()
     <>
     <NavBar/>
     <Welcome/>
+    <CompanyCard/>
+    <JobCard/>
     <Footer/>
     </>
   )
