2e3c6ea64af028fcf7ecd7f532127d9e5e7b37bb
Akash M S <akashmsaiml@gmail.com>
2026-07-01 15:42:34 +0530

    full-stack

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from routers import company,job
+from routers import company,job,auth
 from database import Base,engine
-from models import company as company_model,job as job_model
+from models import company as company_model,job as job_model,users as user_model
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -12,10 +12,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 app.include_router(company.router)
 app.include_router(job.router)
-
+app.include_router(auth.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
