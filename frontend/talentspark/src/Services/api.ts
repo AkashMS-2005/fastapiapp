73fedebbae5fc1e9ad91457eafe42f9e90940cbe
Akash M S <akashmsaiml@gmail.com>
2026-07-10 09:48:47 +0530

    FINAL TOUCH

--- frontend/talentspark/src/Services/api.ts
+++ frontend/talentspark/src/Services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_URL,
     headers: {
         "Content-Type": "application/json",
     },
